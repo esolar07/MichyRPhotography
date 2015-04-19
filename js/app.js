b5b0fdfdb0a4b160afcbd5cab7df21ae814b596d
esolar07 <esolar07@gmail.com>
2015-04-19 02:09:02 -0400

    add more layout styles. add slick

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
+    //Foundation Initializer
 $(document).foundation();
+  
+    // Slick Initializer
+ $(document).ready(function(){
+    $('.autoplay').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        arrows: false,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+    });
+ });
